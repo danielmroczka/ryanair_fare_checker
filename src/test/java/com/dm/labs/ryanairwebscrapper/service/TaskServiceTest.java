1c689bd72b0d2633b54165265c22df796159b1f2
Daniel Mroczka <daniel.mroczka@gmail.com>
2024-05-12 23:21:41 +0200

    wip

--- src/test/java/com/dm/labs/ryanairwebscrapper/service/TaskServiceTest.java
+++ src/test/java/com/dm/labs/ryanairwebscrapper/service/TaskServiceTest.java
@@ -8,7 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.MOCK,
