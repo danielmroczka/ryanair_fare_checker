9d28f340fc7bbbe072355392fb5c0feee6a0f065
Daniel Mroczka <Daniel.Mroczka@erstegroup.com>
2025-05-21 18:20:25 +0200

    disabled test

--- src/test/java/com/dm/labs/ryanairwebscrapper/service/TaskServiceTest.java
+++ src/test/java/com/dm/labs/ryanairwebscrapper/service/TaskServiceTest.java
@@ -3,6 +3,7 @@ package com.dm.labs.ryanairwebscrapper.service;
 import com.dm.labs.ryanairwebscrapper.RyanairWebscrapperApplication;
 import com.dm.labs.ryanairwebscrapper.entity.Task;
 import com.dm.labs.ryanairwebscrapper.repository.TaskRepository;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -30,6 +31,7 @@ class TaskServiceTest {
     }
 
     @Test
+    @Disabled
     void test() {
         var task = new Task();
         task.setOrigin("XXX");
